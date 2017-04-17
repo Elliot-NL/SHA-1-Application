b3b3453ece30512edfacb4d7c9ad45095850a136
Elliot-NL <elliotisvery@hotmail.co.uk>
2017-04-17 13:54:13 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SHA_1_Application
+{
+    class Program
+    {
+        private static string Generator(int hashlength)
+        {
+            const string Hash = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
+            char[] chars = new char[hashlength];
+            Random rd = new Random();
+            for(int i = 0; i<hashlength; i++)
+            {
+                chars[i] = Hash[rd.Next(0, Hash.Length)];
+            }
+            return new string(chars);
+            
+        }
+        static void Main(string[] args)
+        {
+            ///SHA 1 hash is a Secure Hashing Algorithm. 
+            ///It is 160 bits long and Creates a random 
+            ///hash for any piece of data. 
+
+            Console.WriteLine("Enter Hash Length");
+            string uservalue = Console.ReadLine();
+            int pass = Convert.ToInt32(uservalue);
+            
+            Console.WriteLine(Generator(pass));
+            Console.ReadLine();
+        }
+    }
+}
